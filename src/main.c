92447946863a413ebba401af402c97e299e88283
NorthernMan54 <19808920+NorthernMan54@users.noreply.github.com>
2024-10-09 19:26:29 -0400

    Spa Demo

--- src/main.c
+++ src/main.c
@@ -1,26 +1,28 @@
 /**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
-
+ ******************************************************************************
+ * @file    main.c
+ * @author  Ac6
+ * @version V1.0
+ * @date    01-December-2013
+ * @brief   Default main function.
+ ******************************************************************************
+ */
 
 #include "lvgl.h"
 #include "app_hal.h"
+
+#include "ui.h"
 
 #include "demos/lv_demos.h"
 
 int main(void)
 {
-	lv_init();
 
-	hal_setup();
+  lv_init();
 
-  lv_demo_widgets();
+  hal_setup();
 
-	hal_loop();
+  ui_init();
+
+  hal_loop();
 }
